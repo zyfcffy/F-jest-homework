8ab323107080aebe1c38160cf21815c0d6ed91fd
杜娟 <jdu.xian@gmail.com>
2020-06-03 17:47:06 +0800

    add class properties babel support

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ["@babel/preset-env", "airbnb"],
+  plugins: ["@babel/plugin-proposal-class-properties"],
   env: {
     test: {
       presets: [["@babel/preset-env", { targets: { node: "current" } }]],
