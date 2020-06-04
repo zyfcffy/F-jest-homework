33dcf61c61b732573f9402e36ac21d16ac4ce8e6
杜娟 <jdu.xian@gmail.com>
2020-06-04 22:11:04 +0800

    fix babelrc issue for parcel

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ["@babel/preset-env", "airbnb"],
-  plugins: ["@babel/plugin-proposal-class-properties"],
-  env: {
-    test: {
-      presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-    },
-  },
-};
