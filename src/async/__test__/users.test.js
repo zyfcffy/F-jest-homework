8c1cbcc1cca9aa1282519c51362000bc9e4cbe8e
fufangyuan <zyfcffy@163.com>
2020-11-15 19:14:22 +0800

    [fufangyuan]feat:fix bug

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -4,7 +4,7 @@ jest.mock("axios");
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
-    const result = await getUsers();
-    expect(result).toEqual({ name: "ABC" });
+    const result = getUsers();
+    await expect(result).resolves.toEqual({ name: "ABC" });
   });
 });
